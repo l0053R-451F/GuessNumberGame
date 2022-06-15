0462cc7d9f7c0eee8cf7b6e6a16c5bcaf9a493ae
ishafiul <shafiulislam20@gmail.com>
2022-06-16 00:29:44 +0600

    create new game and available game

---
+++ js/client.js
@@ -0,0 +1,56 @@
+let ws
+let gamesAvail
+function heartbeat() {
+    clearTimeout(this.pingTimeout);
+    this.pingTimeout = setTimeout(() => {
+        this.terminate();
+        console.log('dead')
+    }, 300 + 100);
+}
+
+function connect() {
+    //live https://ishaf-ws.herokuapp.com/
+    ws = new WebSocket('ws://localhost:8485/api/v1/ws/game1')
+    ws.addEventListener('ping', heartbeat)
+    ws.addEventListener("open", (res) => {
+        console.log('Connected')
+        const id = localStorage.getItem('id');
+        const payLoad = {
+            'action': 'connect',
+            'username': 'ishaf'
+        }
+        send(payLoad)
+
+        ws.addEventListener("message", (res) => {
+            const data = JSON.parse(res.data)
+            if (data.message === 'gamesAvailResponse') {
+                gamesAvail = data.gameList
+                document.getElementById("gameList").innerHTML= JSON.stringify(gamesAvail);
+            }
+        })
+    })
+
+}
+
+function createNewGame() {
+    const payLoad = {
+        'action': 'create',
+    }
+    send(payLoad)
+}
+
+if (ws) {
+
+    ws.on('close', function clear() {
+        clearTimeout(this.pingTimeout);
+    });
+}
+const id = localStorage.getItem('id');
+if (id) {
+    //document.getElementById('connect').disabled =true
+    console.log('test storage', id) // now need to reconnect with existing id
+}
+
+function send(payLoad) {
+    ws.send(JSON.stringify(payLoad))
+}
--- server.js
+++ server.js
@@ -6,25 +6,34 @@ require("dotenv").config();
 
 const port = process.env.PORT || 8485;
 
-const wss = new WebSocket.Server({ server:server ,path:'/api/v1/ws/game1'});
+const wss = new WebSocket.Server({server: server, path: '/api/v1/ws/game1'});
 
 
 const clients = new Map();
-wss.on('connection', ws=> {
-    const id = uuidv4();
+const gameList = []
+wss.on('connection', ws => {
     ws.isAlive = true;
     ws.on('pong', heartbeat);
     ws.on('message', (messageAsString) => {
+
         const data = JSON.parse(messageAsString)
-        const username =data.username;
-        if (data.action === 'connect' ){
-            const metadata = { id, username};
-            ws.send(JSON.stringify({id,username}))
+
+        if (data.action === 'connect') {
+            const id = uuidv4();
+            ws.uid = id
+            const username = data.username;
+            const metadata = {id, username};
+            ws.send(JSON.stringify({id, username}))
             clients.set(ws, metadata);
-            console.log(clients.get(ws))
+            sendAvailableGameToSelf(ws.uid)
         }
-        if (data.action === 'testmessage'){
-            ws.send(JSON.stringify({message: `connection ok. you send ${data.message}`}))
+        if (data.action === 'create') {
+            const newGameData = {
+                id: Math.floor(1000 + Math.random() * 9000),
+                players: [ws.uid]
+            }
+            gameList.push(newGameData)
+            sendAvailableGameToAll()
         }
     })
 
@@ -33,11 +42,32 @@ wss.on('connection', ws=> {
 const interval = setInterval(function ping() {
     wss.clients.forEach(function each(ws) {
         if (ws.isAlive === false) return ws.terminate();
-
         ws.isAlive = false;
         ws.ping();
     });
 }, 300);
+
+
+function sendAvailableGameToSelf(id) {
+    wss.clients.forEach(ws => {
+        if (ws.uid === id) {
+            ws.send(JSON.stringify({
+                'message': 'gamesAvailResponse',
+                 gameList,
+
+            }))
+        }
+    })
+}
+function sendAvailableGameToAll() {
+    wss.clients.forEach(ws => {
+        ws.send(JSON.stringify({
+            'message': 'gamesAvailResponse',
+            gameList,
+
+        }))
+    })
+}
 
 
 wss.on("close", () => {
@@ -45,13 +75,18 @@ wss.on("close", () => {
 });
 
 function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
+
 function heartbeat() {
     this.isAlive = true;
+}
+
+function send(payload) {
+
 }
 
 app.get('/', (req, res) => res.send('Hello World!'))
